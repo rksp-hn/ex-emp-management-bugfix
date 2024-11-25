4f22492e2425da84ef7ca5a38cf96c20e135179f
HN <hiroyuki.nagashima@rakus-partners.co.jp>
2024-11-25 16:06:41 +0900

    shuusei

--- src/main/java/com/example/form/InsertAdministratorForm.java
+++ src/main/java/com/example/form/InsertAdministratorForm.java
@@ -16,22 +16,19 @@ public class InsertAdministratorForm {
 	/** 名前 */
 	@NotBlank(message = "名前は必須です")
 	// @Sizeで文字数バリデーションを追加しました。
-	@Size(min = 1, max = 30, message = "名前は1文字以上30文字以内で記載してください。")
+	@Size(min = 1, max = 30, message = "1文字以上30文字以内で記載してください")
 	private String name;
 	/** メールアドレス */
-	// ＠NotBlankを追加しました。
-	@NotBlank
-	// @Sizeで文字数バリデーションを追加しました。
-	// メールアドレスの文字数制限を緩和しました。（ネット参照し技術的な上限値に設定しました。）
-	@Size(min = 1, max = 254, message = "メールアドレスは254文字以内で記載してください。")
+	// ＠NotBlankを追加しました。// @Sizeで文字数バリデーションを追加しました。// メールアドレスの文字数制限を緩和しました。（ネット参照し技術的な上限値に設定しました。）
+	@NotBlank(message = "メールアドレスは必須です")
+	@Size(min = 1, max = 254, message = "254文字以内で記載してください")
 	@Email(message = "Eメールの形式が不正です")
 	private String mailAddress;
 	/** パスワード */
-	// ＠NotBlankを追加しました。
-	@NotBlank
-	//パスワードのバリデーションを修正しました。（ネット参照し一般的なセキュリティ条件を満たしているよう設定しました。）
-	@Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*[0-9])[a-zA-Z0-9]{8,}$")
-	@Size(min=8, message = "パスワードは英数字を含む8文字以上で記載してください")
+	// ＠NotBlankを追加しました。//パスワードのバリデーションを修正しました。（ネット参照し一般的なセキュリティ条件を満たしているよう設定しました。）
+	@NotBlank(message = "パスワードは必須です")	
+	@Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*[0-9])[a-zA-Z0-9]{8,}$", message = "半角英数字を含む8文字以上で設定してください" )
+	@Size(min=8, message = "")
 	private String password;
 
 	public String getName() {
